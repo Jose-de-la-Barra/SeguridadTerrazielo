43cd405d3041812465b48db7a3fa29ad341ef9a0
agustindiazad <agudiaaz1@gmail.com>
2023-10-04 02:31:36 -0300

    json

--- proyecto1.py
+++ proyecto1.py
@@ -1,6 +1,6 @@
 import cv2
 from pyzbar.pyzbar import decode
-import re
+import json
 
 # Función para decodificar el código QR
 
@@ -8,9 +8,10 @@ cap = cv2.VideoCapture(1)
 cap.set(3, 640)
 cap.set(4, 480)
 
-found_qr = False  # Variable de control para determinar si se encontró un código QR
+# Variable de control para determinar si se ha detectado un código QR
+qr_detected = False
 
-while not found_qr:
+while not qr_detected:
     success, img = cap.read()
 
     for barcode in decode(img):
@@ -18,7 +19,28 @@ while not found_qr:
         run = myData.split("RUN=")[1]
         run = run.split("&")[0]
         print(run)
-        found_qr = True  # Establece la variable de control en True cuando se encuentra un código QR
+
+        # Cargar los registros existentes desde el archivo JSON si existe
+        try:
+            with open('qr_info.json', 'r') as json_file:
+                qr_info_list = json.load(json_file)
+        except FileNotFoundError:
+            qr_info_list = []
+
+        # Crear un diccionario con la información del código QR
+        qr_info = {
+            'RUN': run,
+            'bandera': '',
+            'comentario': ''
+        }
+
+        qr_info_list.append(qr_info)
+
+        # Guardar la lista actualizada en un archivo JSON
+        with open('qr_info.json', 'w') as json_file:
+            json.dump(qr_info_list, json_file, indent=4)
+
+        qr_detected = True
 
     cv2.imshow('Result', img)
     cv2.waitKey(1)

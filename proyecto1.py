eb8670c191a95f35460dfdd905e22e2d70917d1c
agustindiazad <agudiaaz1@gmail.com>
2023-10-04 01:46:16 -0300

    run

--- proyecto1.py
+++ proyecto1.py
@@ -1,22 +1,28 @@
 import cv2
 from pyzbar.pyzbar import decode
-import numpy as np
+import re
 
 # Función para decodificar el código QR
-#img = cv2.imread('qr.jpg')
 
 cap = cv2.VideoCapture(1)
-cap.set(3,640)
-cap.set(4,480)
+cap.set(3, 640)
+cap.set(4, 480)
 
-while True:
-    
+found_qr = False  # Variable de control para determinar si se encontró un código QR
+
+while not found_qr:
     success, img = cap.read()
+
     for barcode in decode(img):
-        print(barcode.data)
         myData = barcode.data.decode('utf-8')
-        print(myData)
-    
+        run = myData.split("RUN=")[1]
+        run = run.split("&")[0]
+        print(run)
+        found_qr = True  # Establece la variable de control en True cuando se encuentra un código QR
+
     cv2.imshow('Result', img)
     cv2.waitKey(1)
 
+# Liberar la captura de la cámara
+cap.release()
+cv2.destroyAllWindows()
